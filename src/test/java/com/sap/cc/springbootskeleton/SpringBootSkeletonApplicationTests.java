fe1446955cdd3781e856ca7701b3dde5d8be4698
Mark De Vries <mar.de.vries@sap.com>
2020-06-16 15:39:55 +0200

    Skeleton Project for reproducibility

---
+++ src/test/java/com/sap/cc/springbootskeleton/SpringBootSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.cc.springbootskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSkeletonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
